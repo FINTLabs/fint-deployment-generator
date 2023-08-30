5448130b43e78c437fb57d74bf3644156c5b06c5
Hknots <Hknots99@gmail.com>
2023-08-30 09:25:37 +0200

    add spacing between feature elements

--- src/components/FlaisGenerator/FlaisGeneratorForm/FeaturesForm/FeaturesForm.jsx
+++ src/components/FlaisGenerator/FlaisGeneratorForm/FeaturesForm/FeaturesForm.jsx
@@ -5,6 +5,7 @@ import OnePasswordFields from "./OnePasswordFields";
 import DatabaseFields from "./DatabaseFields";
 import UrlFields from "./UrlFields";
 import KafkaFields from "./KafkaFields";
+import {Stack} from "@mui/material";
 
 const FeaturesForm = ({form, setForm, handleChange}) => {
 
@@ -20,50 +21,51 @@ const FeaturesForm = ({form, setForm, handleChange}) => {
 
     return (
         <>
-            <ToggleFieldGroup
-                label="prometheus"
-                isActive={form.prometheus.active}
-                onToggle={() => handleToggle('prometheus')}
-            >
-                <PrometheusFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
-            <ToggleFieldGroup
-                label="onePassword"
-                isActive={form.onePassword.active}
-                onToggle={() => handleToggle('onePassword')}
-            >
-                <OnePasswordFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
-            <ToggleFieldGroup
-                label="database"
-                isActive={form.database.active}
-                onToggle={() => handleToggle('database')}
-            >
-                <DatabaseFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
-            <ToggleFieldGroup
-                label="url"
-                isActive={form.url.active}
-                onToggle={() => handleToggle('url')}
-            >
-                <UrlFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
-            <ToggleFieldGroup
-                label="ingress"
-                isActive={form.ingress.active}
-                onToggle={() => handleToggle('ingress')}
-            >
-                <UrlFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
+            <Stack spacing={2}>
+                <ToggleFieldGroup
+                    label="prometheus"
+                    isActive={form.prometheus.active}
+                    onToggle={() => handleToggle('prometheus')}
+                >
+                    <PrometheusFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
+                <ToggleFieldGroup
+                    label="onePassword"
+                    isActive={form.onePassword.active}
+                    onToggle={() => handleToggle('onePassword')}
+                >
+                    <OnePasswordFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
+                <ToggleFieldGroup
+                    label="database"
+                    isActive={form.database.active}
+                    onToggle={() => handleToggle('database')}
+                >
+                    <DatabaseFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
+                <ToggleFieldGroup
+                    label="url"
+                    isActive={form.url.active}
+                    onToggle={() => handleToggle('url')}
+                >
+                    <UrlFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
+                <ToggleFieldGroup
+                    label="ingress"
+                    isActive={form.ingress.active}
+                    onToggle={() => handleToggle('ingress')}
+                >
+                    <UrlFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
 
-            <ToggleFieldGroup
-                label="kafka"
-                isActive={form.kafka.active}
-                onToggle={() => handleToggle('kafka')}
-            >
-                <KafkaFields form={form} handleChange={handleChange}/>
-            </ToggleFieldGroup>
-
+                <ToggleFieldGroup
+                    label="kafka"
+                    isActive={form.kafka.active}
+                    onToggle={() => handleToggle('kafka')}
+                >
+                    <KafkaFields form={form} handleChange={handleChange}/>
+                </ToggleFieldGroup>
+            </Stack>
         </>
     );
 };
