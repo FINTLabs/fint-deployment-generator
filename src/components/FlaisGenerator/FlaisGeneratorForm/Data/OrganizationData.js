fe058ea9a8961f246be128cb304ce8598e933686
Hknots <Hknots99@gmail.com>
2023-09-06 13:14:53 +0200

    add organization data

---
+++ src/components/FlaisGenerator/FlaisGeneratorForm/Data/OrganizationData.js
@@ -0,0 +1,182 @@
+export const organizationData = {
+    fintlabs: {
+        name: "FINTLabs",
+        active: true
+    },
+    agderfk: {
+        name: "Agder fylkeskommune",
+        active: false
+    },
+    afk: {
+        name: "Akershus fylkeskommune",
+        active: false
+    },
+    arkitektum: {
+        name: "Arkitektum AS",
+        active: false
+    },
+    asker_kommune: {
+        name: "Asker Kommune",
+        active: false
+    },
+    austagderfk: {
+        name: "Aust-Agder fylkeskommune",
+        active: false
+    },
+    bfk: {
+        name: "Buskerud fylkeskommune",
+        active: false
+    },
+    cgi: {
+        name: "CGI Norge AS",
+        active: false
+    },
+    dfo: {
+        name: "DFØ",
+        active: false
+    },
+    evry: {
+        name: "EVRY Norge AS",
+        active: false
+    },
+    ffk: {
+        name: "Finnmark fylkeskommune",
+        active: false
+    },
+    fridiks: {
+        name: "Frid IKS",
+        active: false
+    },
+    hedmarkfk: {
+        name: "Hedmark fylkeskommune",
+        active: false
+    },
+    hordalandfk: {
+        name: "Hordaland fylkeskommune",
+        active: false
+    },
+    innlandetfylke: {
+        name: "Innlandet fylkeskommune",
+        active: false
+    },
+    kristiansand_kommune: {
+        name: "Kristiandsand Kommune",
+        active: false
+    },
+    lybskole: {
+        name: "Longyearbyen videregående skole",
+        active: false
+    },
+    mrfylke: {
+        name: "Møre og Romsdal fylkeskommune",
+        active: false
+    },
+    nfk: {
+        name: "Nordland fylkeskommune",
+        active: false
+    },
+    opplandfk: {
+        name: "Oppland fylkeskommune",
+        active: false
+    },
+    oslo_kommune: {
+        name: "Oslo kommune",
+        active: false
+    },
+    bya_oslo_kommune: {
+        name: "Oslo kommune Byantikvaren",
+        active: false
+    },
+    bym_oslo_kommune: {
+        name: "Oslo kommune Bymiljøetaten",
+        active: false
+    },
+    ude_oslo_kommune: {
+        name: "Oslo kommune Utdanningsetaten",
+        active: false
+    },
+    ra: {
+        name: "Riksantikvaren",
+        active: false
+    },
+    rogfk: {
+        name: "Rogaland fylkeskommune",
+        active: false
+    },
+    samediggi: {
+        name: "Samediggi / Sametinget",
+        active: false
+    },
+    sami_vgs: {
+        name: "Samisk Videregående Skole, Karasjok",
+        active: false
+    },
+    samisk_vgs: {
+        name: "Samisk videregående skole og reindriftsskole",
+        active: false
+    },
+    sfj: {
+        name: "Sogn og Fjordane fylkeskommune",
+        active: false
+    },
+    telemarkfylke: {
+        name: "Telemark fylkeskommune",
+        active: false
+    },
+    telemark: {
+        name: "Telemark fylkeskommune (gammel)",
+        active: false
+    },
+    tromsfylke: {
+        name: "Troms fylkeskommune",
+        active: false
+    },
+    tffk: {
+        name: "Troms og Finnmark fylkeskommune",
+        active: false
+    },
+    trondelagfylke: {
+        name: "Trøndelag fylkeskommune",
+        active: false
+    },
+    vaf: {
+        name: "Vest-Agder fylkeskommune",
+        active: false
+    },
+    vestfoldfylke: {
+        name: "Vestfold fylkeskommune",
+        active: false
+    },
+    vfk: {
+        name: "Vestfold fylkeskommune (gammel)",
+        active: false
+    },
+    vtfk: {
+        name: "Vestfold og Telemark fylkeskommune",
+        active: false
+    },
+    vlfk: {
+        name: "Vestland fylkeskommune",
+        active: false
+    },
+    viken: {
+        name: "Viken fylkeskommune",
+        active: false
+    },
+    visma: {
+        name: "Visma AS",
+        active: false
+    },
+    vismaenterprise: {
+        name: "Visma Enterprise AS",
+        active: false
+    },
+    visma_com: {
+        name: "visma.com",
+        active: false
+    },
+    ofk: {
+        name: "Østfold fylkeskommune",
+        active: false
+    }
+};
