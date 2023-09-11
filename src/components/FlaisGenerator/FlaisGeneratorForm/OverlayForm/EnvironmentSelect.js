46cbac1ec414238c20bf13aef967a42eaae9f2be
Hknots <Hknots99@gmail.com>
2023-09-11 08:19:21 +0200

    add environment selection

---
+++ src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/EnvironmentSelect.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import FormControl from '@mui/material/FormControl';
+import Checkbox from '@mui/material/Checkbox';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+const EnvironmentSelect = () => {
+    const [selectedEnvironments, setSelectedEnvironments] = useState(['Beta']);
+    const environmentNames = ['Api', 'Beta', 'Alpha', 'Pwf'];
+
+    const handleChange = (event, value) => {
+        setSelectedEnvironments(value);
+    };
+
+    const filterOptions = (options, { inputValue }) => {
+        return options.filter(
+            option => option.toLowerCase().includes(inputValue.toLowerCase())
+        );
+    };
+
+    return (
+        <div>
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <Autocomplete
+                    multiple
+                    id="checkboxes-tags-demo"
+                    options={environmentNames}
+                    disableCloseOnSelect
+                    value={selectedEnvironments}
+                    onChange={handleChange}
+                    filterOptions={filterOptions}
+                    getOptionLabel={(option) => option}
+                    isOptionEqualToValue={(option, value) => option === value}
+                    renderOption={(props, option, { selected }) => (
+                        <li {...props}>
+                            <Checkbox style={{ marginRight: 8 }} checked={selected} />
+                            {option}
+                        </li>
+                    )}
+                    renderInput={(params) => (
+                        <TextField {...params} variant="outlined" label="Environments" />
+                    )}
+                />
+            </FormControl>
+        </div>
+    );
+};
+
+export default EnvironmentSelect;
--- src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OverlayForm.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OverlayForm.js
@@ -1,10 +1,12 @@
 import OrganizationSelect from "./OrganizationSelect";
 import React from "react";
+import EnvironmentSelect from "./EnvironmentSelect";
 
 const OverlayForm = () => {
     return (
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-4xl font-bold text-center my-4 mx-2">Overlays</h1>
+            <EnvironmentSelect />
             <OrganizationSelect />
         </div>
     );
