104b8087e6f13613a8d42c86bf4b0d00836beb09
Hknots <Hknots99@gmail.com>
2023-09-11 08:14:58 +0200

    change overlays title

--- src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OverlayForm.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OverlayForm.js
@@ -4,7 +4,7 @@ import React from "react";
 const OverlayForm = () => {
     return (
         <div className="flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-bold text-center my-4 mx-2">Overlays Generator</h1>
+            <h1 className="text-4xl font-bold text-center my-4 mx-2">Overlays</h1>
             <OrganizationSelect />
         </div>
     );
