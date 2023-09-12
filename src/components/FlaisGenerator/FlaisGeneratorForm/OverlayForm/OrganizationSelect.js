817c801949e75139b7c9d0950ffca645ad7a670d
Hknots <Hknots99@gmail.com>
2023-09-12 13:57:06 +0200

    add package & fix warning

--- src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OrganizationSelect.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/OverlayForm/OrganizationSelect.js
@@ -24,7 +24,7 @@ const OrganizationSelect = ({ selectedOrgs, setSelectedOrgs }) => {
         });
 
         setSelectedOrgs(selectedOrgObjects);
-    }, [orgData]);
+    }, [orgData, setSelectedOrgs]);
 
     const handleChange = (event, value) => {
         setSelectedOrgs(
