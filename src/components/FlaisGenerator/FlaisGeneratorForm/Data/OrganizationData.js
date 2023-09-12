28b4902badd26072cc48858e8ed2ecad9ce7ddd5
Hknots <Hknots99@gmail.com>
2023-09-12 12:14:40 +0200

    update organization key names

--- src/components/FlaisGenerator/FlaisGeneratorForm/Data/OrganizationData.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/Data/OrganizationData.js
@@ -1,182 +1,182 @@
 export const organizationData = {
-    fintlabs: {
-        name: "FINTLabs",
-        active: true
+    "agderfk.no": {
+        "name": "Agder fylkeskommune",
+        "active": false
     },
-    agderfk: {
-        name: "Agder fylkeskommune",
-        active: false
+    "afk.no": {
+        "name": "Akershus fylkeskommune",
+        "active": false
     },
-    afk: {
-        name: "Akershus fylkeskommune",
-        active: false
+    "arkitektum.no": {
+        "name": "Arkitektum AS",
+        "active": false
     },
-    arkitektum: {
-        name: "Arkitektum AS",
-        active: false
+    "asker.kommune.no": {
+        "name": "Asker Kommune",
+        "active": false
     },
-    asker_kommune: {
-        name: "Asker Kommune",
-        active: false
+    "austagderfk.no": {
+        "name": "Aust-Agder fylkeskommune",
+        "active": false
     },
-    austagderfk: {
-        name: "Aust-Agder fylkeskommune",
-        active: false
+    "bfk.no": {
+        "name": "Buskerud fylkeskommune",
+        "active": false
     },
-    bfk: {
-        name: "Buskerud fylkeskommune",
-        active: false
+    "cgi.com": {
+        "name": "CGI Norge AS",
+        "active": false
     },
-    cgi: {
-        name: "CGI Norge AS",
-        active: false
+    "dfo.no": {
+        "name": "DFØ",
+        "active": false
     },
-    dfo: {
-        name: "DFØ",
-        active: false
+    "evry.com": {
+        "name": "EVRY Norge AS",
+        "active": false
     },
-    evry: {
-        name: "EVRY Norge AS",
-        active: false
+    "fintlabs.no": {
+        "name": "FINTLabs",
+        "active": true
     },
-    ffk: {
-        name: "Finnmark fylkeskommune",
-        active: false
+    "ffk.no": {
+        "name": "Finnmark fylkeskommune",
+        "active": false
     },
-    fridiks: {
-        name: "Frid IKS",
-        active: false
+    "fridiks.no": {
+        "name": "Frid IKS",
+        "active": false
     },
-    hedmarkfk: {
-        name: "Hedmark fylkeskommune",
-        active: false
+    "hedmark.org": {
+        "name": "Hedmark fylkeskommune",
+        "active": false
     },
-    hordalandfk: {
-        name: "Hordaland fylkeskommune",
-        active: false
+    "hordaland.no": {
+        "name": "Hordaland fylkeskommune",
+        "active": false
     },
-    innlandetfylke: {
-        name: "Innlandet fylkeskommune",
-        active: false
+    "innlandetfylke.no": {
+        "name": "Innlandet fylkeskommune",
+        "active": false
     },
-    kristiansand_kommune: {
-        name: "Kristiandsand Kommune",
-        active: false
+    "kristiansand.kommune.no": {
+        "name": "Kristiandsand Kommune",
+        "active": false
     },
-    lybskole: {
-        name: "Longyearbyen videregående skole",
-        active: false
+    "lybskole.no": {
+        "name": "Longyearbyen videregående skole",
+        "active": false
     },
-    mrfylke: {
-        name: "Møre og Romsdal fylkeskommune",
-        active: false
+    "mrfylke.no": {
+        "name": "Møre og Romsdal fylkeskommune",
+        "active": false
     },
-    nfk: {
-        name: "Nordland fylkeskommune",
-        active: false
+    "nfk.no": {
+        "name": "Nordland fylkeskommune",
+        "active": false
     },
-    opplandfk: {
-        name: "Oppland fylkeskommune",
-        active: false
+    "oppland.no": {
+        "name": "Oppland fylkeskommune",
+        "active": false
     },
-    oslo_kommune: {
-        name: "Oslo kommune",
-        active: false
+    "oslo.kommune.no": {
+        "name": "Oslo kommune",
+        "active": false
     },
-    bya_oslo_kommune: {
-        name: "Oslo kommune Byantikvaren",
-        active: false
+    "bya.oslo.kommune.no": {
+        "name": "Oslo kommune Byantikvaren",
+        "active": false
     },
-    bym_oslo_kommune: {
-        name: "Oslo kommune Bymiljøetaten",
-        active: false
+    "bym.oslo.kommune.no": {
+        "name": "Oslo kommune Bymiljøetaten",
+        "active": false
     },
-    ude_oslo_kommune: {
-        name: "Oslo kommune Utdanningsetaten",
-        active: false
+    "ude.oslo.kommune.no": {
+        "name": "Oslo kommune Utdanningsetaten",
+        "active": false
     },
-    ra: {
-        name: "Riksantikvaren",
-        active: false
+    "ra.no": {
+        "name": "Riksantikvaren",
+        "active": false
     },
-    rogfk: {
-        name: "Rogaland fylkeskommune",
-        active: false
+    "rogfk.no": {
+        "name": "Rogaland fylkeskommune",
+        "active": false
     },
-    samediggi: {
-        name: "Samediggi / Sametinget",
-        active: false
+    "samediggi.no": {
+        "name": "Samediggi / Sametinget",
+        "active": false
     },
-    sami_vgs: {
-        name: "Samisk Videregående Skole, Karasjok",
-        active: false
+    "sami.vgs.no": {
+        "name": "Samisk Videregående Skole, Karasjok",
+        "active": false
     },
-    samisk_vgs: {
-        name: "Samisk videregående skole og reindriftsskole",
-        active: false
+    "samisk.vgs.no": {
+        "name": "Samisk videregående skole og reindriftsskole",
+        "active": false
     },
-    sfj: {
-        name: "Sogn og Fjordane fylkeskommune",
-        active: false
+    "sfj.no": {
+        "name": "Sogn og Fjordane fylkeskommune",
+        "active": false
     },
-    telemarkfylke: {
-        name: "Telemark fylkeskommune",
-        active: false
+    "telemarkfylke.no": {
+        "name": "Telemark fylkeskommune",
+        "active": false
     },
-    telemark: {
-        name: "Telemark fylkeskommune (gammel)",
-        active: false
+    "telemark.no": {
+        "name": "Telemark fylkeskommune (gammel)",
+        "active": false
     },
-    tromsfylke: {
-        name: "Troms fylkeskommune",
-        active: false
+    "tromsfylke.no": {
+        "name": "Troms fylkeskommune",
+        "active": false
     },
-    tffk: {
-        name: "Troms og Finnmark fylkeskommune",
-        active: false
+    "tffk.no": {
+        "name": "Troms og Finnmark fylkeskommune",
+        "active": false
     },
-    trondelagfylke: {
-        name: "Trøndelag fylkeskommune",
-        active: false
+    "trondelagfylke.no": {
+        "name": "Trøndelag fylkeskommune",
+        "active": false
     },
-    vaf: {
-        name: "Vest-Agder fylkeskommune",
-        active: false
+    "vaf.no": {
+        "name": "Vest-Agder fylkeskommune",
+        "active": false
     },
-    vestfoldfylke: {
-        name: "Vestfold fylkeskommune",
-        active: false
+    "vestfoldfylke.no": {
+        "name": "Vestfold fylkeskommune",
+        "active": false
     },
-    vfk: {
-        name: "Vestfold fylkeskommune (gammel)",
-        active: false
+    "vfk.no": {
+        "name": "Vestfold fylkeskommune (gammel)",
+        "active": false
     },
-    vtfk: {
-        name: "Vestfold og Telemark fylkeskommune",
-        active: false
+    "vtfk.no": {
+        "name": "Vestfold og Telemark fylkeskommune",
+        "active": false
     },
-    vlfk: {
-        name: "Vestland fylkeskommune",
-        active: false
+    "vlfk.no": {
+        "name": "Vestland fylkeskommune",
+        "active": false
     },
-    viken: {
-        name: "Viken fylkeskommune",
-        active: false
+    "viken.no": {
+        "name": "Viken fylkeskommune",
+        "active": false
     },
-    visma: {
-        name: "Visma AS",
-        active: false
+    "visma.no": {
+        "name": "Visma AS",
+        "active": false
     },
-    vismaenterprise: {
-        name: "Visma Enterprise AS",
-        active: false
+    "visma.enterprise.as": {
+        "name": "Visma Enterprise AS",
+        "active": false
     },
-    visma_com: {
-        name: "visma.com",
-        active: false
+    "visma.com": {
+        "name": "visma.com",
+        "active": false
     },
-    ofk: {
-        name: "Østfold fylkeskommune",
-        active: false
+    "ofk.no": {
+        "name": "Østfold fylkeskommune",
+        "active": false
     }
-};
+}
