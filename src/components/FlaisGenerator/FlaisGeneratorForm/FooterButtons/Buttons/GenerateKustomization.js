80e98b6a056128fee512e95bf80a05cf79fcbd95
Hknots <Hknots99@gmail.com>
2023-09-12 12:22:19 +0200

    rename folder generated by org

--- src/components/FlaisGenerator/FlaisGeneratorForm/FooterButtons/Buttons/GenerateKustomization.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/FooterButtons/Buttons/GenerateKustomization.js
@@ -2,36 +2,30 @@ import React from "react";
 import { Button } from "@mui/material";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
-import {baseKustomization} from "../../Data/KustomizationFiles";
+import { baseKustomization } from "../../Data/KustomizationFiles";
 
 const GenerateKustomization = ({ yaml, environments, organizations }) => {
     const generateFiles = () => {
         const zip = new JSZip();
 
-        // Create base directory
         const baseFolder = zip.folder("kustomization/base");
 
-        // Create flais.yaml and kustomization.yaml in the base directory
         baseFolder.file("flais.yaml", yaml);
-        baseFolder.file("kustomization.yaml", baseKustomization); // Replace with actual content
+        baseFolder.file("kustomization.yaml", baseKustomization);
 
-        // Create overlays directory
         const overlaysFolder = zip.folder("kustomization/overlays");
 
-        // Iterate through each environment
         environments.forEach((env) => {
             const envFolder = overlaysFolder.folder(env);
 
-            // Iterate through each organization within the environment
             organizations.forEach((org) => {
-                const orgFolder = envFolder.folder(org);
+                const orgFolderName = org.key.replace(/\./g, "-");
+                const orgFolder = envFolder.folder(orgFolderName);
 
-                // Create kustomization.yaml inside the organization directory
-                orgFolder.file("kustomization.yaml", "some content"); // Replace with actual content
+                orgFolder.file("kustomization.yaml", "some content");
             });
         });
 
-        // Generate the ZIP file and trigger the download
         zip.generateAsync({ type: "blob" }).then((blob) => {
             saveAs(blob, "kustomization.zip");
         });
