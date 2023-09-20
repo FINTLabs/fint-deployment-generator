9f700dd1a0e30c4494eb70e88e61041d04e41dbb
Hknots <Hknots99@gmail.com>
2023-09-20 09:02:40 +0200

    rename button files

--- src/components/FlaisGenerator/FlaisGeneratorForm/FooterButtons/FooterButtons.js
+++ src/components/FlaisGenerator/FlaisGeneratorForm/FooterButtons/FooterButtons.js
@@ -1,12 +1,12 @@
-import DisplayFlaisApplication from "./Buttons/DisplayFlaisApplication";
-import GenerateKustomization from "./Buttons/GenerateKustomization";
+import FlaisApplicationButton from "./Buttons/DisplayFlaisApplication";
+import GenerateKustomizeButton from "./Buttons/GenerateKustomization";
 
 const FooterButtons = ({ formData, yaml, setYaml, environments, organizations }) => {
     return (
         <div className="pb-16">
             <div className="fixed bottom-0 left-0 right-0 h-16 flex items-center justify-center z-50 gap-2">
-                <DisplayFlaisApplication formData={formData} yaml={yaml} setYaml={setYaml} />
-                <GenerateKustomization yaml={yaml} formData={formData} environments={environments} organizations={organizations} />
+                <FlaisApplicationButton formData={formData} yaml={yaml} setYaml={setYaml} />
+                <GenerateKustomizeButton yaml={yaml} formData={formData} environments={environments} organizations={organizations} />
             </div>
         </div>
     );
